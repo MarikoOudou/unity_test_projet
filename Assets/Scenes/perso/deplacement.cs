1a6f608df52fc23559521f381e3224207082bef0
MarikoOudou <45321693+MarikoOudou@users.noreply.github.com>
2021-03-13 07:50:07 +0100

    first commit

---
+++ Assets/Scenes/perso/deplacement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class deplacement : MonoBehaviour
+{
+
+    //Variable
+    public float VitesseMarche = 3f;
+    public int CoeffRot = 60;
+    private Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.forward * Input.GetAxis("Vertical") * VitesseMarche * Time.deltaTime);
+    }
+}
---
+++ Assets/deplace.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class deplace : MonoBehaviour
+{
+    //Variable
+    public float VitesseMarche = 3f;
+    public int CoeffRot = 60;
+    private Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.up * Input.GetAxis("Horizontal") * CoeffRot * Time.deltaTime);
+
+        // deplacement avec animation
+        if(Input.GetKey(KeyCode.UpArrow))
+        {
+            // transform.Translate(Vector3.forward * Input.GetAxis("Vertical") * VitesseMarche * Time.deltaTime);
+            anim.SetBool("deplacer", true);
+        } else
+        {
+            anim.SetBool("deplacer", false);
+
+        }
+    }
+}
